49b84f31ca659acc7547c204a6d3296c57a91836
Ranganath2511 <150597821+Ranganath2511@users.noreply.github.com>
2024-11-11 14:42:19 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,12 +1,28 @@
 def decode_message( s: str, p: str) -> bool:
-  def decode_message(s: str, p: str) -> bool:
-    def match_helper(i, j):
-        if j == len(p):
-            return i == len(s) 
-        if j < len(p) and p[j] == '*':
-            return (i < len(s) and match_helper(i + 1, j)) or match_helper(i, j + 1)
-        if i < len(s) and (p[j] == '?' or p[j] == s[i]):
-            return match_helper(i + 1, j + 1)
-        return False
-    return match_helper(0, 0)
-        return False
+
+
+   
+    dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+   
+    dp[0][0] = True
+
+
+    for j in range(1, len(p) + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+
+  
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == '*':
+              
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+            elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+               
+                dp[i][j] = dp[i - 1][j - 1]
+
+   
+    return dp[len(s)][len(p)]
+
+  
+    return False
