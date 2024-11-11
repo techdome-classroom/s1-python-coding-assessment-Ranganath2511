4ada4cc1db8af1156530af2d76133acb6ac97f75
Ranganath2511 <150597821+Ranganath2511@users.noreply.github.com>
2024-11-11 13:49:47 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,42 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
+       import unittest
+from program1 import Solution
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_case1(self):
+        result = self.solution.getTotalIsles([
+            ["L","L","L","L","W"],
+            ["L","L","W","L","W"],
+            ["L","L","W","W","W"],
+            ["W","W","W","W","W"]
+        ])
+        self.assertEqual(result, 1)
+
+    def test_case2(self):
+        result = self.solution.getTotalIsles([
+            ["L","L","W","W","W"],
+            ["L","L","W","W","W"],
+            ["W","W","L","W","W"],
+            ["W","W","W","L","L"]
+        ])
+        self.assertEqual(result, 3)
+
+    def test_case3(self):
+        result = self.solution.getTotalIsles([
+            ["W", "W", "W", "W"],
+            ["W", "L", "L", "W"],
+            ["W", "L", "L", "W"],
+            ["W", "W", "W", "W"]
+        ])
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False)
+   #    write your code here
                     
         return 0
